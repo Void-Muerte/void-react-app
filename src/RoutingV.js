6c98537d69c52d557d42e74794897dc02427412d
Psychology Simango <psicologia5086@gmail.com>
2023-03-10 20:50:56 +0000

    Update RoutingV.js

--- src/RoutingV.js
+++ src/RoutingV.js
@@ -1,8 +1,10 @@
-import {Routes, Route} from 'react-router-dom';
-import {Home, About, Products, Contact} from './pages';
+import { Routes, Route } from "react-router-dom";
+import { Home, About, Products, Contact } from "./pages";
 
-export default function RoutingV(){
-    return(
-        <div></div>
-    );
+export default function RoutingV() {
+  return (
+    <div>
+      <Routes></Routes>
+    </div>
+  );
 }
